fc30b3fe58d4334413bd701ecf3e3a6d35d24f72
Andrii <kollider184@gmail.com>
2022-04-19 18:29:43 +0300

    removed factory method from __init__.

--- app/__init__.py
+++ app/__init__.py
@@ -22,40 +22,35 @@ from .models import Anonymous
 
 login_manager.anonymous_user = Anonymous
 
+#config_class=Config
 
-def create_app(config_class=Config):
-	app = Flask(__name__)
-	app.config.from_object(Config)
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+try:
+	db.create_all()
+except Exception as e:
+	print(e)
+migrate.init_app(app)
+bcrypt.init_app(app)
+login_manager.init_app(app)
+mail.init_app(app)
+from .users.routes import users
+from .delta_flow.routes import delta_flows
+from .main.routes import main
+from .month_plan.routes import month_plans
+from .month_type.routes import month_types
+from .spendings.routes import spendings
 
-	db.init_app(app)
-	try:
-		db.create_all()
-	except Exception as e:
-		print(e)
-	migrate.init_app(app)
-	bcrypt.init_app(app)
-	login_manager.init_app(app)
-	mail.init_app(app)
+from .error.handlers import errors
 
-	from .users.routes import users
-	from .delta_flow.routes import delta_flows
-	from .main.routes import main
-	from .month_plan.routes import month_plans
-	from .month_type.routes import month_types
-	from .spendings.routes import spendings
+app.register_blueprint(users)
+app.register_blueprint(delta_flows)
+app.register_blueprint(main)
+app.register_blueprint(month_plans)
+app.register_blueprint(month_types)
+app.register_blueprint(spendings)
 
-	from .error.handlers import errors
-
-	app.register_blueprint(users)
-	app.register_blueprint(delta_flows)
-	app.register_blueprint(main)
-	app.register_blueprint(month_plans)
-	app.register_blueprint(month_types)
-	app.register_blueprint(spendings)
-
-	app.register_blueprint(errors)
-
-	return app
+app.register_blueprint(errors)
 
 
-app = create_app()
