d63d41195d180dabc6dbdd6bb92936b318067bb7
Andrii <kollider184@gmail.com>
2022-04-19 18:36:50 +0300

    added factory method back to __init__.

--- app/__init__.py
+++ app/__init__.py
@@ -22,35 +22,40 @@ from .models import Anonymous
 
 login_manager.anonymous_user = Anonymous
 
-#config_class=Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db.init_app(app)
-try:
-	db.create_all()
-except Exception as e:
-	print(e)
-migrate.init_app(app)
-bcrypt.init_app(app)
-login_manager.init_app(app)
-mail.init_app(app)
-from .users.routes import users
-from .delta_flow.routes import delta_flows
-from .main.routes import main
-from .month_plan.routes import month_plans
-from .month_type.routes import month_types
-from .spendings.routes import spendings
+def create_app(config_class=Config):
+	app = Flask(__name__)
+	app.config.from_object(Config)
 
-from .error.handlers import errors
+	db.init_app(app)
+	try:
+		db.create_all()
+	except Exception as e:
+		print(e)
+	migrate.init_app(app)
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
 
-app.register_blueprint(users)
-app.register_blueprint(delta_flows)
-app.register_blueprint(main)
-app.register_blueprint(month_plans)
-app.register_blueprint(month_types)
-app.register_blueprint(spendings)
+	from .users.routes import users
+	from .delta_flow.routes import delta_flows
+	from .main.routes import main
+	from .month_plan.routes import month_plans
+	from .month_type.routes import month_types
+	from .spendings.routes import spendings
 
-app.register_blueprint(errors)
+	from .error.handlers import errors
+
+	app.register_blueprint(users)
+	app.register_blueprint(delta_flows)
+	app.register_blueprint(main)
+	app.register_blueprint(month_plans)
+	app.register_blueprint(month_types)
+	app.register_blueprint(spendings)
+
+	app.register_blueprint(errors)
+
+	return app
 
 
+app = create_app()
